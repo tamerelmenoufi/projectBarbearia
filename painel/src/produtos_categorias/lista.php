6258fb49ff5c2c75fa667b4974f4292e0f99f997
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-20 10:11:38 -0400

    Atualizações

--- painel/src/produtos_categorias/lista.php
+++ painel/src/produtos_categorias/lista.php
@@ -71,7 +71,7 @@
                   </td>
                   <td style="white-space: nowrap;">
 
-                    <button class="btn btn-success btn-sm" produtos="<?=$d->codigo?>" nome="<?=$d->categoria?>">
+                    <button class="btn btn-success btn-sm" produtos="<?=$d->codigo?>" categoria="<?=$d->categoria?>">
                     <i class="fa-solid fa-box-open"></i> Produtos
                     </button>
 
