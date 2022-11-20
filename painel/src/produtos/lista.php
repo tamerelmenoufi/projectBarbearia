d2f69d88dc468748c660ad3962f9c24e541aa331
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-20 10:14:37 -0400

    Atualizações

--- painel/src/produtos/lista.php
+++ painel/src/produtos/lista.php
@@ -32,7 +32,7 @@
     <div class="row">
       <div class="col">
         <div class="card">
-          <h5 class="card-header">Lista de Produtos / Serviços <?=$_SESSION['categoriaProdutoNome']?></h5>
+          <h5 class="card-header">Lista de Produtos / Serviços (<?=$_SESSION['categoriaProdutoNome']?>)</h5>
           <div class="card-body">
             <div style="display:flex; justify-content:end">
 
