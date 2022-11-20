7946aa3670dcb02fb013da2500ee4cd498d808eb
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-20 09:03:20 -0400

    Atualizações

--- painel/lib/includes.php
+++ painel/lib/includes.php
@@ -11,5 +11,5 @@
 
     $md5 = md5(date("YmdHis"));
 
-    $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/painel/";
-    $localSite = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/site/";
+    $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/projectBarbearia/painel/";
+    $localSite = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/projectBarbearia/site/";
