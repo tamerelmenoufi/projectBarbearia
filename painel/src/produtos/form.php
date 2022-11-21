bdf280b531a01219b11d700d1435d8b18bcfe3bc
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-21 17:07:16 -0400

    Atualizações

--- painel/src/produtos/form.php
+++ painel/src/produtos/form.php
@@ -98,7 +98,7 @@
       <div class="form-floating mb-3">
         <div class="input-group">
           <span class="input-group-text" id="inputGroup-sizing-default">Valor R$ </span>
-          <input type="number" class="form-control" value="<?=$d->valor?>" id="valor" name="valor">
+          <input type="text" class="form-control" value="<?=$d->valor?>" id="valor" name="valor">
           <span class="input-group-text" id="inputGroup-sizing-default">Estoque</span>
           <div class="form-control"><?=$d->estoque?></div>
         </div>
