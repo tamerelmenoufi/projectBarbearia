cfae88611c086dbe8221843967e6780ab50b8b1f
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-21 17:41:58 -0400

    Atualizações

--- painel/src/produtos/form.php
+++ painel/src/produtos/form.php
@@ -113,7 +113,7 @@
         <div class="form-text">Selecione a imagem para o produto</div>
       </div>
 
-      <button type="submit" data-bs-dismiss="offcanvasXXX" class="btn btn-primary mt-3"> <i class="fa fa-save"></i> Salvar Dados</button>
+      <button type="submit" data-bs-dismiss="offcanvas" class="btn btn-primary mt-3"> <i class="fa fa-save"></i> Salvar Dados</button>
       <button cancelar type="button" data-bs-dismiss="offcanvas" class="btn btn-danger mt-3"> <i class="fa fa-cancel"></i> Cancelar</button>
 
       <input type="hidden" id="acao" name="acao" value="salvar" >
