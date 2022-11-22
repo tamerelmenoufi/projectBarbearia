8cc89d6cc8b35b6e51facfa1dbe65f2ec171ec6a
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-22 11:55:03 -0400

    Atualizações

--- painel/src/estoque/lista.php
+++ painel/src/estoque/lista.php
@@ -73,7 +73,7 @@
                   <!-- <td style="white-space: nowrap;"><?=$d->produto?></td> -->
                   <td style="white-space: nowrap;"><?=$d->titulo?></td>
                   <td style="white-space: nowrap;"><?=dataBr($d->nota_data)?></td>
-                  <td style="white-space: nowrap;">+<?=$d->estoque?></td>
+                  <td style="white-space: nowrap;"><i class="fa-solid fa-plug-circle-xmark" style="color:red"></i> +<?=$d->estoque?></td>
                   <td style="white-space: nowrap;"><a href='<?=$localPainel?>src/volume/estoque/<?=$d->nota?>' target='_blank'><i class="fa-solid fa-arrow-up-right-from-square"></i> Abrir</a></td>
                   <!-- <td style="white-space: nowrap;">
 
