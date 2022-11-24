c966815fcbe7efdc8419c6bd550515cbed807aa1
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-24 14:42:24 -0400

    Atualizações

--- painel/src/clientes/enderecos.php
+++ painel/src/clientes/enderecos.php
@@ -3,32 +3,25 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/portal/painel/lib/includes.php");
 
     if($_POST['acao'] == 'delete'){
-        $query = "update paginas_topicos set topicos = JSON_REMOVE(topicos, '$.titulo[{$_POST['opc']}]'), topicos = JSON_REMOVE(topicos, '$.descricao[{$_POST['opc']}]') where codigo = '{$_POST['cod']}'";
+        $query = "delete from clientes_enderecos where codigo = '{$_POST['cod']}'";
         mysqli_query($con, $query);
     }
 
 
     if($_POST['acao'] == 'insert'){
-        $query = "update paginas_topicos set topicos = JSON_MERGE(topicos, '".json_encode(['titulo' => $_POST['titulo'], 'descricao' => ''], JSON_UNESCAPED_UNICODE)."') where codigo = '{$_POST['cod']}'";
+        $query = "insert into clientes_enderecos set titulo = '{$_POST['titulo']}', cliente = '{$_POST['cliente']}'";
         mysqli_query($con, $query);
     }
 
 
-    $query = "select * from paginas_topicos where codigo = '{$_POST['cod']}'";
+    $query = "select * from clientes_enderecos where cliente = '{$_POST['cliente']}'";
     $result = mysqli_query($con, $query);
-    $d = mysqli_fetch_object($result);
-
-    $topicos = json_decode($d->topicos);
-
-
-
-
 ?>
 
 
 <div class="input-group mt-3 mb-3">
-  <input type="text" class="form-control" id="titulo_topico" placeholder="Digite o título do Tópico" aria-label="Informe o título de novo tópico que deseja cadastrar">
-  <button class="btn btn-success novo" type="button"><i class="fa fa-file"></i> Novo Tópico</button>
+  <input type="text" class="form-control" id="titulo" placeholder="Digite o nome do Endereço" aria-label="Informe o título de seu novo endereço">
+  <button class="btn btn-success novo" type="button"><i class="fa fa-file"></i> Novo Endereço</button>
 </div>
 
 <!-- <button class="btn btn-success novo mt-3 mb-3">
@@ -37,13 +30,13 @@
 
 <ul class="list-group">
 <?php
-    foreach($topicos->titulo as $i => $val){
+    while($d = mysqli_fetch_object($result)){
 ?>
     <li class="list-group-item opc" style="display: flex; justify-content: space-between;">
-        <div class="titulo"><?=$val?></div>
+        <div class="titulo"><?=$d->titulo?></div>
         <div>
-            <button class="btn btn-primary btn-sm editar" cod="<?=$i?>"><i class="fa-regular fa-pen-to-square"></i></button>
-            <button class="btn btn-danger btn-sm excluir" cod="<?=$i?>"><i class="fa-solid fa-trash-can"></i></button>
+            <button class="btn btn-primary btn-sm editar" cod="<?=$d->codigo?>"><i class="fa-regular fa-pen-to-square"></i></button>
+            <button class="btn btn-danger btn-sm excluir" cod="<?=$d->codigo?>"><i class="fa-solid fa-trash-can"></i></button>
         </div>
     </li>
 <?php
@@ -56,11 +49,11 @@
         Carregando('none');
 
         $("button.novo").click(function(){
-            titulo = $("#titulo_topico").val();
+            titulo = $("#titulo").val();
             if(titulo){
-                $("#titulo_topico").val('');
+                $("#titulo").val('');
                 $.ajax({
-                    url:"src/paginas_topicos/topicos.php",
+                    url:"src/clientes_enderecos/enderecos.php",
                     type:"POST",
                     data:{
                         cod:'<?=$_POST['cod']?>',
@@ -80,7 +73,7 @@
             opc = $(this).attr("cod");
 
             $.ajax({
-                url:"src/paginas_topicos/topicos_form.php",
+                url:"src/clientes_enderecos/enderecos_form.php",
                 type:"POST",
                 data:{
                     cod:'<?=$_POST['cod']?>',
@@ -96,11 +89,11 @@
 
         $("button.excluir").click(function(){
             cod = $(this).attr("cod");
-            topico = $(this).parent("div").parent("li").children("div.titulo").text();
+            titulo = $(this).parent("div").parent("li").children("div.titulo").text();
 
             // $.alert(cod);
             $.confirm({
-                content:`Deseja realmente excluir o tópico:<br><b>${topico}</b>?`,
+                content:`Deseja realmente excluir o endereço:<br><b>${titulo}</b>?`,
                 title:false,
                 type:'red',
                 buttons:{
@@ -109,11 +102,10 @@
                         btnClass:'btn btn-danger',
                         action:function(){
                             $.ajax({
-                                url:"src/paginas_topicos/topicos.php",
+                                url:"src/clientes_enderecos/enderecos.php",
                                 type:"POST",
                                 data:{
-                                    cod:'<?=$_POST['cod']?>',
-                                    opc:cod,
+                                    cod,
                                     acao:'delete',
                                 },
                                 success:function(dados){
