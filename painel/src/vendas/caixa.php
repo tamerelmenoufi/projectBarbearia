032a7d92787c9aa84340e7e8b55ae7a0b02f4659
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-25 10:11:20 -0400

    Atualizações

---
+++ painel/src/vendas/caixa.php
@@ -0,0 +1,16 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
+?>
+<style>
+
+</style>
+
+<h5>Dados do caixa</h5>
+
+<script>
+    $(function(){
+        Carregando('none');
+
+
+    })
+</script>
---
+++ painel/src/vendas/index.php
@@ -0,0 +1,23 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
+?>
+<style>
+
+</style>
+<div class="p-3">
+    <div class="row">
+        <div lista class="col-md-12"></div>
+    </div>
+</div>
+
+<script>
+    $(function(){
+        Carregando('none');
+        $.ajax({
+            url:"src/vendas/caixa.php",
+            success:function(dados){
+                $("div[lista]").html(dados);
+            }
+        });
+    })
+</script>
