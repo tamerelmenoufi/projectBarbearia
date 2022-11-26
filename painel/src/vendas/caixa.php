bf973be51f810ca6c55025a258824266de9bd7ba
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-26 08:38:54 -0400

    Atualizações

--- painel/src/vendas/caixa.php
+++ painel/src/vendas/caixa.php
@@ -3,7 +3,7 @@
 ?>
 <style>
     .Categoria-principal{
-        width:100%; position:relative; overflow:auto;
+        width:100%; position:absolute; overflow:auto;
     }
     .Categoria-principal.elementos{
         width:auto; height:60px; padding:20px; border:1px red solid; position:relative; white-space:nowrap; float:left; margin:10px;
