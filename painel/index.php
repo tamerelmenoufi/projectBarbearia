d0b5c8c1cd95873eedbd71ac139d85a95a4ecd31
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-26 07:54:24 -0400

    Atualizações

--- painel/index.php
+++ painel/index.php
@@ -18,7 +18,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="img/logo.png">
+    <link rel="icon" type="image/x-icon" href="img/icone.png">
     <title>Project</title>
     <?php
     include("lib/header.php");
