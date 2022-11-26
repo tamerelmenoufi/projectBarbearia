a10b7e965a6f5cdc1e73138151b33dade64260bf
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-26 11:28:13 -0400

    Atualizações

--- painel/src/vendas/caixa.php
+++ painel/src/vendas/caixa.php
@@ -40,6 +40,40 @@
 </div>
 
 
+<div class="row">
+    <div class="col-md-6">
+
+    <?php
+    for($i=0;$i<10;$i++){
+    ?>
+
+        <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+            </div>
+        </div>
+        </div>
+    <?php
+    }
+    ?>
+
+    </div>
+
+    <div class="col-md-6">
+
+    </div>
+</div>
+
+
+
 <script>
     $(function(){
         Carregando('none');
