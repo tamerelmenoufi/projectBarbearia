5b351589195cbe1a7c383e790cec78ed173d5e21
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-26 09:38:32 -0400

    Atualizações

--- painel/src/vendas/caixa.php
+++ painel/src/vendas/caixa.php
@@ -2,14 +2,26 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
 ?>
 <style>
-
+    .elementos{
+        width:auto;
+        height:60px;
+        padding:20px;
+        border:1px red solid;
+        position:relative;
+        white-space:nowrap;
+        float:left;
+        margin:5px;
+        cursor:pointer;
+    }
 </style>
 <h5>Dados do caixa</h5>
 <div class="main-carousel">
     <?php
         for($i=0;$i<30;$i++){
     ?>
-    <div class="carousel-cell"> Coluna <?=$i?></div>
+    <div class="carousel-cell elementos">
+        Coluna <?=$i?>
+    </div>
     <?php
         }
     ?>
