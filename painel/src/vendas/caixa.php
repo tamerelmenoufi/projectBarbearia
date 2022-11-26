deb850502e4ea63342512e3d29005c0ec8914d15
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-26 08:38:04 -0400

    Atualizações

--- painel/src/vendas/caixa.php
+++ painel/src/vendas/caixa.php
@@ -2,19 +2,22 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
 ?>
 <style>
-
+    .Categoria-principal{
+        width:100%; position:relative; overflow:auto;
+    }
+    .Categoria-principal.elementos{
+        width:auto; height:60px; padding:20px; border:1px red solid; position:relative; white-space:nowrap; float:left; margin:10px;
+    }
 </style>
 <h5>Dados do caixa</h5>
-<div style="width:100%; position:absolute; overflow:auto;">
-    <div class="row">
+<div class="Categoria-principal">
     <?php
         for($i=0;$i<30;$i++){
     ?>
-    <div class="col"> Coluna <?=$i?></div>
+    <div class="elementos"> Coluna <?=$i?></div>
     <?php
         }
     ?>
-    </div>
 </div>
 
 
