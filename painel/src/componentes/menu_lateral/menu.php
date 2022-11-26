a1cce3eb0e97eae454f3f3146173b7101759bf29
tamerelmenoufi <tamer@mohatron.com.br>
2022-11-26 08:01:24 -0400

    Atualizações

--- painel/src/componentes/menu_lateral/menu.php
+++ painel/src/componentes/menu_lateral/menu.php
@@ -3,7 +3,7 @@
 ?>
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <img src="img/logo.png" style="height:60px;" alt="">
+    <img src="img/logo-topo.png" style="height:100px;" alt="">
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
