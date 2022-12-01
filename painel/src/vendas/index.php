9c446162d789e087baba98ff3d883df9666465a3
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-01 16:43:00 -0400

    Atualizações

--- painel/src/vendas/index.php
+++ painel/src/vendas/index.php
@@ -5,7 +5,7 @@
 
 </style>
 <div class="p-3">
-    <div class="row" style="position:relative; border:solid 1px red">
+    <div class="row" style="position:relative; height:100%; border:solid 1px red">
         <div lista class="col-md-12"  style="position:absolute; top:0; left:0; right:0; bottom:0; border:solid 1px red"></div>
     </div>
 </div>
