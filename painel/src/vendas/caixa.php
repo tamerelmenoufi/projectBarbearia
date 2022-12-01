7975979cd852a42edbbd11122f81e5fce92ce1aa
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-01 16:51:22 -0400

    Atualizações

--- painel/src/vendas/caixa.php
+++ painel/src/vendas/caixa.php
@@ -38,39 +38,39 @@
     ?>
 </div>
 
+<div style="position:fixed; top:100px; bottom:0; overflow:auto;">
+    <div class="row">
+        <div class="col-md-6">
 
-<div class="row">
-    <div class="col-md-6">
+        <?php
+        for($i=0;$i<10;$i++){
+        ?>
 
-    <?php
-    for($i=0;$i<10;$i++){
-    ?>
-
-        <div class="card mb-3">
-            <div class="row g-0">
-                <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="...">
-                </div>
-                <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="..." class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                    </div>
                 </div>
             </div>
+        <?php
+        }
+        ?>
+
         </div>
-    <?php
-    }
-    ?>
 
-    </div>
+        <div class="col-md-6">
 
-    <div class="col-md-6">
-
+        </div>
     </div>
 </div>
-
 
 
 <script>
