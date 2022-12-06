d62968ebce1a3b47eeb012c27daf92c6a508360c
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-06 09:20:37 -0400

    Atualizações

--- painel/src/vendas/compras.php
+++ painel/src/vendas/compras.php
@@ -2,7 +2,7 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
 ?>
 
-<h5>Lista de compras</h5>
+<h5>Carrinho de compras</h5>
 
 <script>
     $(function(){
