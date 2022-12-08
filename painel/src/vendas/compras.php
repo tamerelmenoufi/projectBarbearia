74e37b1822b205a116ce360242f40da5d6c9382e
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-08 16:52:18 -0400

    Atualizações

--- painel/src/vendas/compras.php
+++ painel/src/vendas/compras.php
@@ -11,6 +11,37 @@
 
 Meu código de Compra é <?=$_SESSION['codVenda']?>
 
+
+<table class="table table-hover">
+
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Descrição</th>
+            <th scope="col">tipo</th>
+            <th scope="col">Valor</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $query = "select * from vendas_produtos where venda = '{$_SESSION['codVenda']}'";
+        $result = mysqli_query($con, $query);
+        while($d = mysqli_fetch_object($result)){
+        ?>
+        <tr>
+            <th scope="row"><?=$d->codigo?></th>
+            <td><?=$d->produto?></td>
+            <td><?=$d->produto_tipo?></td>
+            <td><?=$d->valor?></td>
+        </tr>
+        <?php
+        }
+        ?>
+    </tbody>
+
+</table>
+
+
 <script>
     $(function(){
         Carregando('none')
