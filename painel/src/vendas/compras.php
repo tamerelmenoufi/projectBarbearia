582d9b906b3c1ce7c40de89a97f9bf6009864fdf
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-11 13:59:40 -0400

    Atualizações

--- painel/src/vendas/compras.php
+++ painel/src/vendas/compras.php
@@ -12,59 +12,63 @@
 
 
 ?>
-
+<style>
+    td{
+        weight-space: nowrap;
+    }
+</style>
 <h5>Carrinho de compras</h5>
 
 Meu código de Compra é <?=$_SESSION['codVenda']?>
 
-
-<table class="table table-hover">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Descrição</th>
-            <th scope="col">Quantidade</th>
-            <th scope="col">Valor Unitário</th>
-            <th scope="col">Valor Total</th>
-            <th scope="col">Ações</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        $query = "select
-                        a.*,
-                        p.codigo as cod_produto,
-                        p.produto as produto_nome,
-                        if(p.tipo = 'p', 'Produto', 'Serviço') as tipo_nome,
-                        c.categoria as categoria_nome
-                    from vendas_produtos a
-                         left join produtos p on a.produto = p.codigo
-                         left join produtos_categorias c on p.categoria = c.codigo
-                    where a.venda = '{$_SESSION['codVenda']}'";
-        $result = mysqli_query($con, $query);
-        while($d = mysqli_fetch_object($result)){
-        ?>
-        <tr>
-            <th scope="row"><?=$d->cod_produto?></th>
-            <td><b><?=$d->produto_nome?></b><br><small><?=$d->categoria_nome?> (<?=$d->tipo_nome?>)</small></td>
-            <td>
-                <button menos="<?=$d->codigo?>" type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-minus"></i></button>
-                <span qt="<?=$d->codigo?>" class="m-3"><?=$d->quantidade?></span>
-                <button mais="<?=$d->codigo?>" type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-plus"></i></button>
-            </td>
-            <td><?=$d->valor_unitario?></td>
-            <td><?=$d->valor?></td>
-            <td>
-                <button type="button" class="btn btn-sm btn-secondary"><i class="fa-solid fa-clipboard-user"></i></button>
-                <button type="button" class="btn btn-sm btn-danger"><i class="fa-regular fa-trash-can"></i></button>
-            </td>
-        </tr>
-        <?php
-        }
-        ?>
-    </tbody>
-</table>
-
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Descrição</th>
+                <th scope="col">Quantidade</th>
+                <th scope="col">Valor Unitário</th>
+                <th scope="col">Valor Total</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $query = "select
+                            a.*,
+                            p.codigo as cod_produto,
+                            p.produto as produto_nome,
+                            if(p.tipo = 'p', 'Produto', 'Serviço') as tipo_nome,
+                            c.categoria as categoria_nome
+                        from vendas_produtos a
+                            left join produtos p on a.produto = p.codigo
+                            left join produtos_categorias c on p.categoria = c.codigo
+                        where a.venda = '{$_SESSION['codVenda']}'";
+            $result = mysqli_query($con, $query);
+            while($d = mysqli_fetch_object($result)){
+            ?>
+            <tr>
+                <th scope="row"><?=$d->cod_produto?></th>
+                <td><b><?=$d->produto_nome?></b><br><small><?=$d->categoria_nome?> (<?=$d->tipo_nome?>)</small></td>
+                <td>
+                    <button menos="<?=$d->codigo?>" type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-minus"></i></button>
+                    <span qt="<?=$d->codigo?>" class="m-3"><?=$d->quantidade?></span>
+                    <button mais="<?=$d->codigo?>" type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-plus"></i></button>
+                </td>
+                <td><?=$d->valor_unitario?></td>
+                <td><?=$d->valor?></td>
+                <td>
+                    <button type="button" class="btn btn-sm btn-secondary"><i class="fa-solid fa-clipboard-user"></i></button>
+                    <button type="button" class="btn btn-sm btn-danger"><i class="fa-regular fa-trash-can"></i></button>
+                </td>
+            </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
 
 <script>
     $(function(){
