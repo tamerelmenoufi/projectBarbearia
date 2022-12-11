8f5fad10df26f1088fed0c28ef2cdfb5942211e6
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-11 10:55:23 -0400

    Atualizações

--- painel/src/vendas/compras.php
+++ painel/src/vendas/compras.php
@@ -30,7 +30,7 @@ Meu código de Compra é <?=$_SESSION['codVenda']?>
                         if(p.tipo = 'p', 'Produto', 'Serviço') as tipo_nome,
                         c.categoria as categoria_nome
                     from vendas_produtos a
-                         left join produtos p on a.produto = b.codigo
+                         left join produtos p on a.produto = p.codigo
                          left join produtos_categorias c on p.categoria = c.codigo
                     where a.venda = '{$_SESSION['codVenda']}'";
         $result = mysqli_query($con, $query);
