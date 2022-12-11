58bbe50eac2b6f069d917653c5ab573cb60530a5
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-11 10:51:05 -0400

    Atualizações

--- painel/src/vendas/compras.php
+++ painel/src/vendas/compras.php
@@ -24,13 +24,21 @@ Meu código de Compra é <?=$_SESSION['codVenda']?>
     </thead>
     <tbody>
         <?php
-        $query = "select * from vendas_produtos where venda = '{$_SESSION['codVenda']}'";
+        $query = "select
+                        a.*,
+                        p.produto as nome_produto,
+                        if(p.tipo = 'p', 'Produto', 'Serviço') as tipo_nome
+                        c.categoria as categoria_nome
+                    from vendas_produtos a
+                         left join produtos p on a.produto = b.codigo
+                         left join produtos_categorias c on p.categoria = c.codigo
+                    where a.venda = '{$_SESSION['codVenda']}'";
         $result = mysqli_query($con, $query);
         while($d = mysqli_fetch_object($result)){
         ?>
         <tr>
             <th scope="row"><?=$d->codigo?></th>
-            <td><?=$d->produto?></td>
+            <td><b><?=$d->produto_nome?></b><br><small><?=$d->categoria_nome?> (<?=$d->tipo_nome?>)</small></td>
             <td><?=$d->produto_tipo?></td>
             <td><?=$d->valor?></td>
         </tr>
