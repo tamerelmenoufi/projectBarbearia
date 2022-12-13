50395ffb064a1113eeb165dac8790be6c8dfa67f
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-13 11:23:48 -0400

    Atualizações

--- painel/src/vendas/compras.php
+++ painel/src/vendas/compras.php
@@ -219,7 +219,7 @@
         </div>
     </div>
 
-    <p>O produto será entregue para: <b><?=$_SESSION['ClienteAtivoNome']?></b> <?=(($d->local_entrega)?"({$d->endereco})":false)?></p>
+    <p style="text-align:center; font-size:10px; color:#a1a1a1;">O produto será entregue para: <b><?=$_SESSION['ClienteAtivoNome']?></b> <?=(($d->local_entrega)?"({$d->endereco})":false)?></p>
 </div>
 
 
