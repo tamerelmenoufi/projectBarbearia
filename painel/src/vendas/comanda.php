9215c634a412d068a8e2053d35a6e3766a973c60
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-15 16:32:11 -0400

    Atualizações

--- painel/src/vendas/comanda.php
+++ painel/src/vendas/comanda.php
@@ -10,86 +10,86 @@
 <!-- <ul class="list-group">
 
 <li class="list-group-item"> -->
+<div class="p-3">
+    <div class="row justify-content-between" style="margin-bottom:10px;">
+    <div class="col-2">
+        <b>Cod</b>
+    </div>
+    <div class="col-4">
+        <b>Descrição</b>
+    </div>
+    <div class="col-2">
+        <b>Vl Uni</b>
+    </div>
+    <div class="col-2">
+        <b>Quant.</b>
+    </div>
+    <div class="col-2">
+        <b>Vl Tot</b>
+    </div>
+    </div>
 
-<div class="row justify-content-between">
-  <div class="col-2">
-    Cod
-  </div>
-  <div class="col-4">
-    Descrição
-  </div>
-  <div class="col-2">
-    Vl Uni
-  </div>
-  <div class="col-2">
-    Quant.
-  </div>
-  <div class="col-2">
-    Vl Tot
-  </div>
+    <?php
+        $query = "select
+                                a.*,
+                                p.tipo,
+                                p.codigo as cod_produto,
+                                p.produto as produto_nome,
+                                if(p.tipo = 'p', 'Produto', 'Serviço') as tipo_nome,
+                                c.categoria as categoria_nome
+                            from vendas_produtos a
+                                left join produtos p on a.produto = p.codigo
+                                left join produtos_categorias c on p.categoria = c.codigo
+                            where a.venda = '{$_SESSION['codVenda']}'";
+                $result = mysqli_query($con, $query);
+                $n = mysqli_num_rows($result);
+                $valor = $comissao = 0;
+                $tipo_produtos = false;
+                while($d = mysqli_fetch_object($result)){
+    ?>
+
+
+    <div class="row justify-content-between">
+    <div class="col-2">
+
+    </div>
+    <div class="col-10">
+        <b><?=$d->produto_nome?></b><br><small><?=$d->categoria_nome?> (<?=$d->tipo_nome?>)</small>
+    </div>
+    </div>
+
+    <div class="row justify-content-between" style="margin-bottom:20px;">
+    <div class="col-2">
+        <?=$d->cod_produto?>
+    </div>
+    <div class="col-4">
+
+    </div>
+    <div class="col-2">
+        R$ <?=number_format($d->valor_unitario,2,',','.')?>
+    </div>
+    <div class="col-2">
+        <?=$d->quantidade?>
+    </div>
+    <div class="col-2">
+        R$ <?=number_format($d->valor,2,',','.')?>
+    </div>
+    </div>
+
+    <?php
+                }
+    ?>
+
+    <div class="row justify-content-between" style="margin-top:10px;">
+    <div class="col-10 text-right">
+        <b>TOTAL</b>
+    </div>
+
+    <div class="col-2">
+        <b>R$ <?=number_format($v->total,2,',','.')?></b>
+    </div>
+    </div>
 </div>
-
-<?php
-    $query = "select
-                            a.*,
-                            p.tipo,
-                            p.codigo as cod_produto,
-                            p.produto as produto_nome,
-                            if(p.tipo = 'p', 'Produto', 'Serviço') as tipo_nome,
-                            c.categoria as categoria_nome
-                        from vendas_produtos a
-                            left join produtos p on a.produto = p.codigo
-                            left join produtos_categorias c on p.categoria = c.codigo
-                        where a.venda = '{$_SESSION['codVenda']}'";
-            $result = mysqli_query($con, $query);
-            $n = mysqli_num_rows($result);
-            $valor = $comissao = 0;
-            $tipo_produtos = false;
-            while($d = mysqli_fetch_object($result)){
-?>
-
-
-<div class="row justify-content-between">
-  <div class="col-2">
-
-  </div>
-  <div class="col-10">
-    <b><?=$d->produto_nome?></b><br><small><?=$d->categoria_nome?> (<?=$d->tipo_nome?>)</small>
-  </div>
-</div>
-
-<div class="row justify-content-between" style="margin-bottom:20px;">
-  <div class="col-2">
-    <?=$d->cod_produto?>
-  </div>
-  <div class="col-4">
-
-  </div>
-  <div class="col-2">
-    R$ <?=number_format($d->valor_unitario,2,',','.')?>
-  </div>
-  <div class="col-2">
-    <?=$d->quantidade?>
-  </div>
-  <div class="col-2">
-    R$ <?=number_format($d->valor,2,',','.')?>
-  </div>
-</div>
-
-<?php
-            }
-?>
-
-<div class="row justify-content-between" style="margin-top:10px;">
-  <div class="col-10 text-right">
-    <b>TOTAL</b>
-  </div>
-
-  <div class="col-2">
-    <b>R$ <?=number_format($v->total,2,',','.')?></b>
-  </div>
-</div>
-
 
   <!-- </li>
 </ul> -->
