f2bf136af055fab87ba0878a49a263b3201a7dec
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-16 08:09:39 -0400

    Atualizações

--- painel/src/clientes/enderecos_entrega.php
+++ painel/src/clientes/enderecos_entrega.php
@@ -21,14 +21,11 @@
     .titulo{
         cursor:pointer;
     }
-    #retirada_estabelecimento{
-        cursor:pointer;
-    }
 </style>
 <h4 class="Titulo<?=$md5?>">Lista de Endereços</h4>
 
 
-<div class="mb-3 form-check">
+<div class="mb-3 form-check" style="cursor:pointer;">
     <input type="checkbox" class="form-check-input" id="retirada_estabelecimento">
     <label class="form-check-label" for="retirada_estabelecimento">Retirada dos produtos no estabelecimento</label>
 </div>
