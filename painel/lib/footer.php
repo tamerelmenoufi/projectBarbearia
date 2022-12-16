d240ac0d07bccecf6ecb78f4fcb75c1ebcd0664c
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-16 17:02:38 -0400

    Atualizações

--- painel/lib/footer.php
+++ painel/lib/footer.php
@@ -1,4 +1,4 @@
-<script src="lib/vendor/bootstrap-5.2.0/js/bootstrap.bundle.min.js"></script>
+<script src="<?=$localPainel?>lib/vendor/bootstrap-5.2.0/js/bootstrap.bundle.min.js"></script>
 
 <script
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSnblPMOwEdteX5UPYXf7XUtJYcbypx6w&language=pt&region=BR"
