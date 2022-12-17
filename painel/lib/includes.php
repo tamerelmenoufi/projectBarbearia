378a23ecb24fb42285336e5b5f556a846adb679e
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-17 08:36:30 -0400

    Atualizações

--- painel/lib/includes.php
+++ painel/lib/includes.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    // include("connect_local.php");
+    include("/appinc/cBarb.php");
     $md5 = md5(date("YmdHis"));
 
     $localPainel = $_SERVER["REQUEST_SCHEME"]."://".$_SERVER["HTTP_HOST"]."/app/projectBarbearia/painel/";
