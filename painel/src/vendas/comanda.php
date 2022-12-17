f342bd4a775e8f857591fde96357ecb04276df3a
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-17 11:37:30 -0400

    Atualizações

--- painel/src/vendas/comanda.php
+++ painel/src/vendas/comanda.php
@@ -218,7 +218,7 @@
             </li>
             <?php
             }
-            $resto = ($total - $nPagamento);
+            $resto = number_format(($total - $nPagamento),2,'.',false);
             ?>
             </ul>
 
