e2d93e5ec9434c3ea209d4a5eef06257e3c183f2
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-17 11:47:34 -0400

    Atualizações

--- painel/src/vendas/comanda.php
+++ painel/src/vendas/comanda.php
@@ -236,7 +236,7 @@
         }
         ?>
         <div class="col d-grid">
-            <button class="btn btn-primary btn-sm"  <?=((!$resto)?'concluir_venda':'disabled')?>><i class="fa-regular fa-circle-check"></i> Concluir</button>
+            <button class="btn btn-primary btn-sm"  <?=((!$resto)?'disabled':'concluir_venda')?>><i class="fa-regular fa-circle-check"></i> Concluir</button>
         </div>
     </div>
 
