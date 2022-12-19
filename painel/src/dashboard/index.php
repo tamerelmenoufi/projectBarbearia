7e798a4692bbbb2b19d6faa6e1eb72b4a0e31735
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-19 08:08:17 -0400

    Atualizações

--- painel/src/dashboard/index.php
+++ painel/src/dashboard/index.php
@@ -2,11 +2,24 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
     vl(['ProjectPainel']);
 ?>
+
+<div class="row">
+    <div class="col">
+        <div class="card p1" vendasProducao></div>
+    </div>
+</div>
+
+
 <script>
     $(function(){
 
         Carregando('none');
-
+        $.ajax({
+            url:"src/dashboard/vendas/producao.php",
+            success:function(dados){
+                $("div[vendasProducao]").html(dados);
+            }
+        });
 
     })
 </script>
---
+++ painel/src/dashboard/vendas/producao.php
@@ -0,0 +1,72 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
+?>
+<h5>Pedidos em Produção</h5>
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Descrição</th>
+                <th scope="col">Quantidade</th>
+                <th scope="col">Valor Unitário</th>
+                <th scope="col">Valor Total</th>
+                <th scope="col">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $query = "select
+                            a.*,
+                            p.tipo,
+                            p.codigo as cod_produto,
+                            p.produto as produto_nome,
+                            if(p.tipo = 'p', 'Produto', 'Serviço') as tipo_nome,
+                            c.categoria as categoria_nome
+                        from vendas_produtos a
+                            left join produtos p on a.produto = p.codigo
+                            left join produtos_categorias c on p.categoria = c.codigo
+                        where a.venda = '{$_SESSION['codVenda']}'";
+            $result = mysqli_query($con, $query);
+            $n = mysqli_num_rows($result);
+            $valor = $comissao = 0;
+            $tipo_produtos = false;
+            if($n) $colaborador = true;
+            while($d = mysqli_fetch_object($result)){
+                if($d->tipo == 'p') $tipo_produtos = true;
+                if(!$d->colaborador) $colaborador = false;
+            ?>
+            <tr>
+                <th scope="row"><?=$d->cod_produto?></th>
+                <td><b><?=$d->produto_nome?></b><br><small><?=$d->categoria_nome?> (<?=$d->tipo_nome?>)</small></td>
+                <td>
+                    <button menos="<?=$d->codigo?>" produto="<?=$d->cod_produto?>" colaborador="<?=$d->colaborador?>" type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-minus"></i></button>
+                    <span qt="<?=$d->codigo?>" class="m-3"><?=$d->quantidade?></span>
+                    <button mais="<?=$d->codigo?>" produto="<?=$d->cod_produto?>" colaborador="<?=$d->colaborador?>" type="button" class="btn btn-sm btn-outline-secondary"><i class="fa-solid fa-plus"></i></button>
+                </td>
+                <td><?=$d->valor_unitario?></td>
+                <td><?=$d->valor?></td>
+                <td>
+                    <button
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        href="#offcanvasDireita"
+                        role="button"
+                        aria-controls="offcanvasDireita"
+                        class="btn btn-sm btn-<?=(($d->colaborador)?'success':'secondary')?> colaborador"
+                        codigo = "<?=$d->codigo?>"
+                        produto = "<?=$d->cod_produto?>"
+                    ><i class="fa-solid fa-clipboard-user"></i></button>
+                    <button type="button" class="btn btn-sm btn-danger excluir" codigo="<?=$d->codigo?>" produto="<?=$d->produto_nome?>"><i class="fa-regular fa-trash-can"></i></button>
+                </td>
+            </tr>
+            <?php
+                // Dados para calculo das somas totais e das comissões
+                $valor = $valor + $d->valor;
+                $comissao = $comissao + $d->comissao;
+            }
+
+            ?>
+        </tbody>
+    </table>
+</div>
