7b5ff44aeeb874c44553b8495723a567d91f7f2a
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-19 08:50:16 -0400

    Atualizações

--- painel/src/dashboard/vendas/producao.php
+++ painel/src/dashboard/vendas/producao.php
@@ -30,8 +30,8 @@
                 <td><?=$d->data_pedido?></td>
                 <td>R$ <?=number_format($d->valor, 2, ",",".")?></td>
                 <td>
-                    <i class="fa-solid fa-clipboard-user ml-3"></i>
-                    <i class="fa-regular fa-trash-can"></i>
+                    <i class="fa-solid fa-clipboard-user"></i>
+                    <i class="fa-regular fa-trash-can ms-3"></i>
                 </td>
             </tr>
             <?php
