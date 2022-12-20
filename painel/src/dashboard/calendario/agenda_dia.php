7feb1c0b85a7360a15a6df752cf3b1417f8ea86f
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-20 11:44:59 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_dia.php
+++ painel/src/dashboard/calendario/agenda_dia.php
@@ -3,7 +3,14 @@
 ?>
 
 <style>
-
+    .agendamento{
+        margin-left:10px;
+        padding:3px;
+        border-radius:5px;
+        background-color:rgb(125,11,234, 0.7);
+        color:#fff;
+        font-size:9px;
+    }
 </style>
 
 <div class="p-1" style="font-size:12px;">
@@ -16,7 +23,8 @@
             $hora = str_pad($i , 2 , '0' , STR_PAD_LEFT).":".str_pad($h , 2 , '0' , STR_PAD_LEFT);
     ?>
     <li class="list-group-item">
-        <?=$hora?>
+        <i class="fa-solid fa-calendar-day"></i> <?=$hora?>
+        <span class="agendamento">José Ribamar<br> <i>Eduardo Fernandes</i> </span>
     </li>
     <?php
         }
