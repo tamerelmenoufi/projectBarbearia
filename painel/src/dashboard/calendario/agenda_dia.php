929c314b1020d0ee62116dbb021450185b9d2eec
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-20 09:31:50 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_dia.php
+++ painel/src/dashboard/calendario/agenda_dia.php
@@ -1,7 +1,31 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
 ?>
-<div class="p-1" style="position:relative; height:350px; overflow:auto; width:100%; clear:both;">
+
+<style>
+
+.element {
+  scrollbar-width: thin;
+  scrollbar-color: black transparent;
+}
+
+.element::-webkit-scrollbar {
+  width: 3px;
+  height: 3px; /* A altura só é vista quando a rolagem é horizontal */
+}
+
+.element::-webkit-scrollbar-track {
+  background: transparent;
+  padding: 2px;
+}
+
+.element::-webkit-scrollbar-thumb {
+  background-color: #000;
+}
+
+</style>
+
+<div class="p-1 element" style="position:relative; height:350px; overflow:auto; width:100%; clear:both;">
     <h6>Agenda do dia</h6>
     <ul class="list-group">
     <?php
