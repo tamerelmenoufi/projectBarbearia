0cfacd39e25f5734caa34c710747e84fae9c74bb
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-20 13:23:36 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_dia.php
+++ painel/src/dashboard/calendario/agenda_dia.php
@@ -24,7 +24,7 @@
     ?>
     <li class="list-group-item">
         <i class="fa-solid fa-calendar-day"></i> <?=$hora?>
-        <div agendamento style="float:left; margin-left:50px; border:red 1px solid" >
+        <div agendamento style="position:absolute; left:50px; right:10px; height:auto; border:red 1px solid" >
             <span class="agendamento">José Ribamar<br> <i>Eduardo Fernandes</i> </span>
         </div>
     </li>
