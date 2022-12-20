b4bee93c115031412fde07b7e5fba55e630815fb
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-20 09:55:05 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_dia.php
+++ painel/src/dashboard/calendario/agenda_dia.php
@@ -4,28 +4,9 @@
 
 <style>
 
-.element {
-  scrollbar-width: thin;
-  scrollbar-color: black transparent;
-}
-
-.element::-webkit-scrollbar {
-  width: 3px;
-  height: 3px; /* A altura só é vista quando a rolagem é horizontal */
-}
-
-.element::-webkit-scrollbar-track {
-  background: transparent;
-  padding: 2px;
-}
-
-.element::-webkit-scrollbar-thumb {
-  background-color: #ccc;
-}
-
 </style>
 
-<div class="p-1 element" style="position:relative; height:350px; overflow:auto; width:100%; clear:both;">
+<div class="p-1">
     <h6>Agenda do dia</h6>
     <ul class="list-group">
     <?php
--- painel/src/dashboard/index.php
+++ painel/src/dashboard/index.php
@@ -4,16 +4,37 @@
 ?>
 
 <style>
+    .element {
+    height:350px;
+    overflow:auto;
+    width:100%;
+    clear:both;
+    scrollbar-width: thin;
+    scrollbar-color: black transparent;
+    }
 
+    .element::-webkit-scrollbar {
+    width: 3px;
+    height: 3px; /* A altura só é vista quando a rolagem é horizontal */
+    }
+
+    .element::-webkit-scrollbar-track {
+    background: transparent;
+    padding: 2px;
+    }
+
+    .element::-webkit-scrollbar-thumb {
+    background-color: #ccc;
+    }
 </style>
 
 <div style="padding:20px;">
     <div class="row">
         <div class="col-md-6">
-            <div class="card p-1" vendasProducao></div>
+            <div class="card p-1 element" vendasProducao></div>
         </div>
         <div class="col-md-6">
-            <div class="card p-1" agendaDia></div>
+            <div class="card p-1 element" agendaDia></div>
         </div>
     </div>
 </div>
