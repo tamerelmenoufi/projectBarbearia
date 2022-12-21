0e22310fc2d4d5cdeb3e4035cddc3088000693b5
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-21 08:06:34 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_cadastro.php
+++ painel/src/dashboard/calendario/agenda_cadastro.php
@@ -10,6 +10,40 @@
         z-index:0;
     }
 </style>
-<h4 class="Titulo<?=$md5?>"><?=date("d/m/Y H:i")?></h4>
+<h4 class="Titulo<?=$md5?>"><?=$_SESSION['agenda_dia']?> <?=$_POST['data']?></h4>
 
-Consulta da agenda de número <?=$_POST['data']?>
+<div class="row">
+    <div class="col-12">
+        <select name="cliente" id="cliente">
+            <option value="1">Nome do cliente</option>
+            <option value="1">Nome do cliente</option>
+            <option value="1">Nome do cliente</option>
+            <option value="1">Nome do cliente</option>
+            <option value="1">Nome do cliente</option>
+        </select>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <select name="colaborador" id="colaborador">
+            <option value="1">Nome do colaborador</option>
+            <option value="1">Nome do colaborador</option>
+            <option value="1">Nome do colaborador</option>
+            <option value="1">Nome do colaborador</option>
+            <option value="1">Nome do colaborador</option>
+        </select>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <textarea name="observacao" id="observacao" class="form-control" cols="30" rows="10"></textarea>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-12">
+        <button class="btn btn-primary">Cadastrar agenda</button>
+    </div>
+</div>
