01ed76638db7dae96bec9ac8c470c116b6626f8a
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-21 08:25:27 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_cadastro.php
+++ painel/src/dashboard/calendario/agenda_cadastro.php
@@ -61,7 +61,8 @@
 <script>
     $(function(){
 
-        $("#cliente, #colaboradores").selectpicker();
+        $("#cliente").selectpicker();
+        $("#colaboradores").selectpicker();
 
 
     })
