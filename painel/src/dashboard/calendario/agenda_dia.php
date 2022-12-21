4fdee182457e7b41c972df370004e3ed15b3fa87
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-20 20:44:47 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_dia.php
+++ painel/src/dashboard/calendario/agenda_dia.php
@@ -18,7 +18,7 @@
 </style>
 
 <div class="p-1" style="font-size:12px;">
-    <div class="row">
+    <div class="row mb-3">
         <div class="col text-start"><h6>Agenda do dia</h6></div>
         <div class="col text-end"><input type="date" id="dataAgenda" class="form-control form-control-sm" /></div>
     </div>
