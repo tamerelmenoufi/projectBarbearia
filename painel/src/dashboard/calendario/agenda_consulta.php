0abaa3cfc6675763fffab6ec5710cc8a886739d3
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-21 11:43:57 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_consulta.php
+++ painel/src/dashboard/calendario/agenda_consulta.php
@@ -30,13 +30,13 @@
         font-size:9px
     }
 </style>
-<h4 class="Titulo<?=$md5?>"><?=dataBr($d->data_agenda)?></h4>
+<h4 class="Titulo<?=$md5?>"><i class="fa-solid fa-calendar-day"></i> <?=dataBr($d->data_agenda)?></h4>
 
 <div class="card mh-3 p-3">
     <div class="row">
         <div class="col dados">
-            <h6><i class="fa-solid fa-user-clock"></i> <?=$d->servico_nome?></h6>
-            <p class="identificacao"><i class="fa-solid fa-user"></i> <?=$d->cliente_nome?><br><span><i class="fa-regular fa-user"></i> Atendimento por: <?=$d->colaborador_nome?></span></p>
+            <h6><i class="fa-solid fa-scissors"></i> <?=$d->servico_nome?></h6>
+            <p class="identificacao"><i class="fa-solid fa-user-clock"></i> <?=$d->cliente_nome?><br><span><i class="fa-solid fa-user"></i> Atendimento por: <?=$d->colaborador_nome?></span></p>
             <p><i class="fa-solid fa-circle-info"></i> <?=$d->observacao?></p>
         </div>
     </div>
