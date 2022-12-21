6db6b681b39f6042de777d6a9b19460e73ec4613
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-21 07:48:13 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_consulta.php
+++ painel/src/dashboard/calendario/agenda_consulta.php
@@ -1,1 +1,15 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
+?>
+
+<style>
+    .Titulo<?=$md5?>{
+        position:absolute;
+        left:60px;
+        top:8px;
+        z-index:0;
+    }
+</style>
+<h4 class="Titulo<?=$md5?>">Editar Endereço</h4>
+
 Consulta da agenda de número <?=$_POST['codigo']?>
