fb663964b7b973bc403e591e432f0a082939e474
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-21 08:34:35 -0400

    Atualizações

--- painel/lib/header.php
+++ painel/lib/header.php
@@ -31,6 +31,3 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/bootstrap-select.min.js"></script>
-
-<!-- (Optional) Latest compiled and minified JavaScript translation files -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/i18n/defaults-*.min.js"></script>
