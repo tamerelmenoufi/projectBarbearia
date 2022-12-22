b61fa3657b5c351aa6f760fa05dbbac1c41be86e
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-22 08:33:01 -0400

    Atualizações

--- painel/src/dashboard/calendario/agenda_cadastro.php
+++ painel/src/dashboard/calendario/agenda_cadastro.php
@@ -13,7 +13,7 @@
                         colaboradores_produtos a
                         left join produtos b on a.produto = b.codigo
                         left join produtos_categorias c on b.categoria = c.codigo
-                    where a.colaborador = '{$c}'";
+                    where a.colaborador = '{$c}' order by c.categoria, b.produto";
         $result = mysqli_query($con, $query);
         $grupo = false;
         while($d = mysqli_fetch_object($result)){
