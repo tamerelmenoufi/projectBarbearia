abf4428f3eb483f19f6b782727eaf5ffc4ec0abf
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-26 22:13:35 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -1,8 +1,8 @@
 
 <nav class="navbar bg-light">
   <div class="container-fluid">
-    <div class="d-flex justify-content-center">
-        <div>MEU LOGIN</div>
+    <div class="d-flex flex-row justify-content-center">
+        <div class="d-flex justify-content-center">MEU LOGIN</div>
     </div>
   </div>
 </nav>
