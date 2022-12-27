229cd09216795e1706aed6ed0edad44c9d1ba05f
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-26 22:23:47 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -1,4 +1,4 @@
-<div class="d-flex flex-row justify-content-center" style="background-color:#eee; height:50px;">
+<div class="d-flex flex-row justify-content-center" style="background-color:#eee; height:70px;">
   <div class="m-3">
     <img id="profile-img" style="width:90px;" class="profile-img-card" src="img/icone.png" />
   </div>
