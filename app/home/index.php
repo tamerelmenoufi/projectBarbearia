c0c4d20aa82fd0d82171b045e43fc517498c77aa
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-26 22:19:29 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -1,5 +1,7 @@
 <div class="d-flex flex-row justify-content-center mb-3">
-  <div>Flex item 1</div>
+  <div>
+    <img id="profile-img" class="profile-img-card" src="img/icone.png" />
+  </div>
 </div>
 
 <!-- <h6>APLICATIVO</h6> -->
