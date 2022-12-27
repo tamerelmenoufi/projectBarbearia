ed2960fc94850deb5a9d860ad2a8a5ba3670a695
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-27 09:13:47 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -1,29 +1,39 @@
 
 
-<div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+<div
+    class="offcanvas offcanvas-end"
+    data-bs-backdrop="static"
+    tabindex="-1"
+    id="offcanvasDireita"
+    aria-labelledby="offcanvasDireitaLabel"
+    style="--bs-offcanvas-width:500px;"
+  >
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body">
-    <div>
-      Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-    </div>
-    <div class="dropdown mt-3">
-      <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
-        Dropdown button
-      </button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-      </ul>
-    </div>
-  </div>
+  <div class="offcanvas-body LateralDireita"></div>
 </div>
 
 
+<script>
 
+
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    $("#offcanvasDireita").css("--bs-offcanvas-width","100%")
+  }
+ else {
+    $("#offcanvasDireita").css("--bs-offcanvas-width","600px")
+  }
+
+
+</script>
 
 <div class="d-flex flex-row justify-content-between" style="background-color:#eee; height:70px; position:fixed; left:0; top:0; width:100%; z-index:9;">
   <div></div>
@@ -139,8 +149,8 @@
                                             href="#"
                                             class="btn btn-outline-secondary"
                                             data-bs-toggle="offcanvas"
-                                            data-bs-target="#offcanvasExample"
-                                            aria-controls="offcanvasExample"
+                                            data-bs-target="#offcanvasDireita"
+                                            aria-controls="offcanvasDireita"
                                         >Go somewhere</a>
                                     </small>
                                 </div>
