eb4de7aeb01fc38bccf435a0ca51dcddd17af354
tamerelmenoufi <tamer@mohatron.com.br>
2022-12-26 22:07:23 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -1,4 +1,10 @@
 
+<nav class="navbar bg-light">
+  <div class="container-fluid">
+    <div class="d-flex justify-content-center">MEU LOGIN</div>
+  </div>
+</nav>
+
 <h6>APLICATIVO</h6>
 
 <script>
