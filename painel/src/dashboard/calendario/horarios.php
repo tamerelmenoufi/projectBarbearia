79e9c3b0bd0a1e70e9c91c291d72a70c9377bb50
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-11 09:06:04 -0400

    Atualizações

--- painel/src/dashboard/calendario/horarios.php
+++ painel/src/dashboard/calendario/horarios.php
@@ -8,5 +8,11 @@
     $inter_ini = strtotime(date("Y-m-d {$hi}:00"));
     $inter_fim = strtotime(date("Y-m-d {$hf}:00"));
 
-    echo "<p>{$inter_ini} até {$inter_fim}</p>";
+    // echo "<p>{$inter_ini} até {$inter_fim}</p>";
 
+    // for($i = $inter_ini; $i <= $inter_fim; $i = $i + )
+
+    $inter_ini = strtotime(date("Y-m-d 10:00:00"));
+    $inter_fim = strtotime(date("Y-m-d 10:01:00"));
+    echo "<p>{$inter_ini} até {$inter_fim}</p>";
+    echo ($inter_fim - $inter_ini);
