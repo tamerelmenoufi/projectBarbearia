570d66cd976ed747dda6539dfa1711ebeff32a32
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-12 08:26:58 -0400

    Atualizações

--- painel/src/dashboard/calendario/horarios.php
+++ painel/src/dashboard/calendario/horarios.php
@@ -3,12 +3,116 @@
     vl(['ProjectPainel']);
     $s = mysqli_fetch_object(mysqli_query($con, "select * from produtos where codigo = '{$_POST['servico']}'"));
 
-
     $dia = json_decode($_SESSION['PeriodoLoja']->dias_horas_atendimento);
     $hj = $abrevSem[date("D")];
     list($hi, $hf) = explode("-",$dia->$hj);
     $inter_ini = strtotime(date("Y-m-d {$hi}:00"));
     $inter_fim = strtotime(date("Y-m-d {$hf}:00"));
+
+
+
+	if($_GET[OpAno] and $_GET[OpMes]){
+		$ano = $_GET[OpAno];
+		$mes = $_GET[OpMes];
+	}else{
+		$ano = date(Y);
+		$mes = date(m);
+	}
+
+?>
+
+<table class='table' cellpadding="5" cellspacing="0" border="0" align="center">
+      <tr>
+          <td colspan="7" align="left" class="titulo">
+        Registro de envios em <select id="OpMes">
+            <option value="">M&eacute;s</option>
+            <?php
+                for($i=1;$i<=12;$i++){
+            ?>
+                <option value="<?=$i?>" <?=(($i == $mes) ? 'selected' : false)?>><?=$i?></option>
+            <?php
+                }
+            ?>
+        </select>
+        /
+        <select id="OpAno">
+            <option value="">Ano</option>
+            <?php
+			for($i = date(Y); $i > date(Y)-4; $i--){
+			?>
+            <option value="<?=$i?>" <?=(($i == $ano) ? 'selected' : false)?>><?=$i?></option>
+            <?php
+			}
+			?>
+<!--            <option value="2012" <?=(('2012' == $ano) ? 'selected' : false)?>>2012</option>
+            <option value="2013" <?=(('2013' == $ano) ? 'selected' : false)?>>2013</option>
+            <option value="2014" <?=(('2014' == $ano) ? 'selected' : false)?>>2014</option>
+-->        </select>
+        </td>
+      </tr>
+<?php
+
+       for($w=$mes;$w<=$mes;$w++){
+		   set_time_limit(90);
+			 $w = (($w*1 < 10)?'0'.$w*1:$w);
+			 $d1 = mktime(0,0,0, $w, 1, $ano); //verifica o primeiro dia do mes
+			 $diaSem = date('w',$d1); //verifica a quantidade de dias da semana para o primeiro dia do mes
+
+?>
+
+			  <tr align='center' class='dias_semana'>
+   				  <td class='lista_titulo' width="14%">Domingo<td class='lista_titulo' width="14%">Segunda<td class='lista_titulo' width="14%">Ter&ccedil;a<td class='lista_titulo' width="14%">Quarta<td class='lista_titulo' width="14%">Quinta<td class='lista_titulo' width="14%">Sexta<td class='lista_titulo' width="14%">S&aacute;bado</td>
+              </tr>
+
+			  <tr>
+
+			<!--Coloca os dias em Branco-->
+			<?php
+                        for ($i = 0; $i < $diaSem; $i++) {
+                        echo "<td geral>&nbsp;";
+                        }
+
+                    //Enquanto houver dias
+
+                        for ($i = 2; $i < 33; $i++) {
+							$linha = date('d',$d1);
+
+
+                    //verifica o dia atual
+
+					    if(date(Y) == $ano and date(m) == $w and date(d) == $linha){
+                           $hoje = ' (HOJE)';
+                        }else{
+                           $hoje = false;
+                        }
+
+
+						// list($qtd) = @mysql_fetch_row(@mysql_query("select count(codigo) from t".$_SESSION[sms_usuario_logado]."_".$ano.$w." where data like '%$ano-$w-$linha%'"));
+
+						//echo "select count(codigo) from t".$_SESSION[sms_usuario_logado]."_".$ano.$w." where data like '%$ano-$w-$linha%'";
+
+						echo "<td geral class='lista_agenda' valign='top' cel>";
+
+
+                        echo "$linha ".$hoje;
+						//$qtd = '354';
+						if($qtd) {
+						    echo "<div ".$qt." data='$ano-$w-$linha'>".$qtd."</div>";
+						}
+						echo "</td>";
+					    // Se Sábado desce uma linha
+                        if (date('w',$d1) == 6) {
+                            echo "<tr>\n";
+                        }
+                        $d1 = mktime(0,0,0, $w, $i, $ano);
+                        if (date('d',$d1) == "01") { break; }
+                   }
+          ?>
+    		</tr>
+	  </table>
+
+<?php
+       }
 
     echo "<p>Colab.: {$_POST['colaborador']} - Serv.: {$_POST['servico']}</p>";
 
@@ -20,3 +124,4 @@
     $inter_fim = strtotime(date("Y-m-d 10:01:00"));
     echo "<p>{$inter_ini} até {$inter_fim}</p>";
     echo ($inter_fim - $inter_ini);
+?>
