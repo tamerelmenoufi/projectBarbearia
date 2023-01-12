729967e7e31846bc3ffdc27adba28a23d41d847b
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-12 10:41:34 -0400

    Atualizações

--- painel/src/dashboard/calendario/horarios.php
+++ painel/src/dashboard/calendario/horarios.php
@@ -121,7 +121,7 @@
 						echo "<td>";
 
                         echo "$linha ".$hoje;
-                        echo "<div class='d-flex align-items-center align-content-center ".(($qtd)?'text-danger':'agenda text-success')."' data='$ano-$w-$linha'><i class='fa-solid fa-circle'></i></div>";
+                        echo "<div class='d-flex align-items-center justify-content-center ".(($qtd)?'text-danger':'agenda text-success')."' data='$ano-$w-$linha'><i class='fa-solid fa-circle'></i></div>";
 
                         echo "</td>";
 					    // Se Sábado desce uma linha
