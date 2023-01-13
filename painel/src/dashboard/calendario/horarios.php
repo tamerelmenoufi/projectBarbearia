dfe65cfb26ece956c23ecda1e93fb94a72bb0180
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-13 15:46:41 -0400

    Atualizações

--- painel/src/dashboard/calendario/horarios.php
+++ painel/src/dashboard/calendario/horarios.php
@@ -51,7 +51,9 @@
         /* opacity:0.5;
         background-color:red; */
     }
-
+    .calendario td div:houver{
+        background-color:rgb(255,123,87, 0.5);
+    }
 </style>
 <div class='card'>
     <table class='table calendario' cellpadding="5" cellspacing="0">
