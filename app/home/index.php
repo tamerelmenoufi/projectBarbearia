4fbb987eef26f3c6c5e2c2d7f914389211d2da14
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-16 15:39:15 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -46,11 +46,11 @@
 
 
 <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel" style="margin-top:70px;">
-  <div class="carousel-indicators">
+  <!-- <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+  </div> -->
   <div class="carousel-inner">
     <div class="carousel-item active" data-bs-interval="10000">
     <img src="img/banner_site.jpg" class="w-100 d-none d-md-block" >
