e8743512a29132ab89acbbcfff3c9390a7f59c52
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-16 10:29:53 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -186,7 +186,7 @@
                   <div class="card-img-overlay">
                     <div class="d-flex align-items-end h-100">
                       <div>
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title"><?=$d->nome?></h5>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <p class="card-text"><small>Last updated 3 mins ago</small></p>
                       </div>
