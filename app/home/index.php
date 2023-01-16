78e94b6389019d24b3efc27a2dd85eac5c67e7f5
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-16 10:40:24 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -175,7 +175,7 @@
 
             <div class="row">
               <?php
-              $query = "select * from colaboradores order by rand()";
+              $query = "select * from colaboradores where codigo > 1 order by rand()";
               $result = mysqli_query($con, $query);
               while($d = mysqli_fetch_object($result)){
               // for($i = 0;$i < 6; $i++){
