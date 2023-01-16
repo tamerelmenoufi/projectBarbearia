6ef23155907730559c6e2dcf75aec3c1c9bf3c96
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-16 10:24:29 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -214,7 +214,7 @@
     </div>
 </div>
 
-<footer class="text-muted py-5 bg-secondary">
+<footer class="text-muted py-5" style="background-color:#2f2e2e">
   <div class="container">
     <p class="float-end mb-1">
       <a href="#">Back to top</a>
