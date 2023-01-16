66bce985cf5b86d0133d1956d5ae3735d415ed34
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-16 11:51:09 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -242,6 +242,7 @@
     <h3>Os Manos Barbearia</h3>
     <p class="mb-1">Av. Djalma Batista, 370 - Chapada</p>
     <p class="float-end mb-0 text-white">
+      <i class="fa-brands fa-whatsapp"></i>
       <i class="fa-brands fa-square-facebook"></i>
       <i class="fa-brands fa-square-twitter"></i>
       <i class="fa-brands fa-square-whatsapp"></i>
