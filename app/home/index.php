36525d6c48dfbf477e50107ffef9f5a1cb3c2f79
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-17 21:59:27 -0400

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -44,101 +44,17 @@
 </div> -->
 
 
-<!-- <nav class="navbar bg-body-tertiary fixed-top" style="background-color:#583b1e; height:70px; left:0; top:0; width:100%; z-index:9;">
+<nav class="navbar bg-body-tertiary fixed-top" style="background-color:#583b1e; height:70px; left:0; top:0; width:100%; z-index:9;">
   <div class="container-fluid d-flex flex-row justify-content-between">
     <img id="profile-img" style="width:90px;" class="profile-img-card" src="img/icone.png" />
-    <span>Nome teste</span>
-  </div>
-</nav> -->
-
-
-
-  <!-- ======= Header ======= -->
-  <header id="header" class="header fixed-top" data-scrollto-offset="0">
-    <div class="container-fluid d-flex align-items-center justify-content-between">
-
-      <a href="index.php" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
-        <img src="assets/img/logo.png" style="height:100px !important;" alt="">
-        <h1 ><span>.Project .</span></h1>
-      </a>
-
-      <nav id="navbar" class="navbar">
-
-      <ul>
-
-          <li class="dropdown">
-            <a href="#"><span>Home</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="index.html" class="active">Home 1 - index.html</a></li>
-              <li><a href="index-2.html">Home 2 - index-2.html</a></li>
-              <li><a href="index-3.html">Home 3 - index-3.html</a></li>
-              <li><a href="index-4.html">Home 4 - index-4.html</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="index.html#about">About</a></li>
-          <li><a class="nav-link scrollto" href="index.html#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="index.html#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="index.html#team">Team</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li>
-                <a href="#">Column 1 link 1</a>
-                <a href="#">Column 1 link 2</a>
-                <a href="#">Column 1 link 3</a>
-              </li>
-              <li>
-                <a href="#">Column 2 link 1</a>
-                <a href="#">Column 2 link 2</a>
-                <a href="#">Column 3 link 3</a>
-              </li>
-              <li>
-                <a href="#">Column 3 link 1</a>
-                <a href="#">Column 3 link 2</a>
-                <a href="#">Column 3 link 3</a>
-              </li>
-              <li>
-                <a href="#">Column 4 link 1</a>
-                <a href="#">Column 4 link 2</a>
-                <a href="#">Column 4 link 3</a>
-              </li>
-            </ul>
-          </li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="index.html#contact">Contact</a></li>
-          <li acao="adm/menu/index.php" target="popup"><i class="bi bi-gear" style="font-size:25px; color:red"></i></li>
-
-        </ul>
-
-        <i class="bi bi-list mobile-nav-toggle d-none"></i>
-
-      <!-- .navbar -->
-      <?php
-
-      ?>
-      <i class="bi bi-list mobile-nav-toggle d-none"></i>
-      </nav>
-      <!-- <a class="btn-getstarted scrollto" href="index.php#about"><i class="fa-regular fa-hand-pointer"></i> Iniciar</a> -->
-
+    <div class="text-white">
+        <span>Serviços</span>
+        <span>Produtos</span>
+        <span>Equipe</span>
     </div>
-  </header>
+  </div>
+</nav>
+
 
 
 
