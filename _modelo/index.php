b404e43c747d3ac6e84073cda6ddff7c892db9a9
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-18 10:28:56 -0400

    Atualizações

--- _modelo/index.php
+++ _modelo/index.php
@@ -72,7 +72,7 @@
     <h5 id="offcanvasRightLabel">Offcanvas right</h5>
     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body">
+  <div class="offcanvas-body LateralDireita">
     ...
   </div>
 </div>
