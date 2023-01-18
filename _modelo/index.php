d8faa002acb2ceb1f6103c32c3e0d033651233a2
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-18 09:21:48 -0400

    Atualizações

--- _modelo/index.php
+++ _modelo/index.php
@@ -75,7 +75,7 @@
 <?php
 
     $componentes = [
-        // 'menu',
+         'menu',
 
         'video',
         // 'banner_principal',
@@ -84,7 +84,7 @@
         // 'banner_principal3',
         // 'sobre',
         // 'noticias',
-        'galeria',
+        // 'galeria',
         // 'banner_depoimentos',
 
         // 'pagina_interna',
