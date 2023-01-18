243e70c1147a79f81bbf715e30cd0e9705b7d0b2
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-18 10:24:30 -0400

    Atualizações

--- _modelo/index.php
+++ _modelo/index.php
@@ -78,19 +78,19 @@
 </div>
 <script>
 
-if( navigator.userAgent.match(/Android/i)
- || navigator.userAgent.match(/webOS/i)
- || navigator.userAgent.match(/iPhone/i)
- || navigator.userAgent.match(/iPad/i)
- || navigator.userAgent.match(/iPod/i)
- || navigator.userAgent.match(/BlackBerry/i)
- || navigator.userAgent.match(/Windows Phone/i)
- ){
-    $("#offcanvasDireita").css("--bs-offcanvas-width","100%")
-  }
- else {
-    $("#offcanvasDireita").css("--bs-offcanvas-width","600px")
-  }
+// if( navigator.userAgent.match(/Android/i)
+//  || navigator.userAgent.match(/webOS/i)
+//  || navigator.userAgent.match(/iPhone/i)
+//  || navigator.userAgent.match(/iPad/i)
+//  || navigator.userAgent.match(/iPod/i)
+//  || navigator.userAgent.match(/BlackBerry/i)
+//  || navigator.userAgent.match(/Windows Phone/i)
+//  ){
+//     $("#offcanvasDireita").css("--bs-offcanvas-width","100%")
+//   }
+//  else {
+//     $("#offcanvasDireita").css("--bs-offcanvas-width","600px")
+//   }
 
 </script>
 
