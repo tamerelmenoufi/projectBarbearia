740129130bd7621f66aa099a5b14ac282f473731
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-18 17:11:45 -0400

    Atualizações

--- _modelo/components/produtos_servicos.php
+++ _modelo/components/produtos_servicos.php
@@ -1,16 +1,22 @@
 <!-- ======= Featured Services Section ======= -->
 <section id="servicos" class="featured-services">
       <div class="container">
-        <div class="section-header">
-          <h2>Serviços</h2>
-        </div>
+
         <div class="row gy-4">
         <?php
               $query = "select a.*, b.categoria as categoria_nome from produtos a left join produtos_categorias b on a.categoria = b.codigo where a.situacao = '1' order by a.tipo desc";
               $result = mysqli_query($con, $query);
               $titulo = false;
               while($d = mysqli_fetch_object($result)){
-            ?>
+                if($titulo != $d->categoria){
+        ?>
+                  <div class="section-header">
+                    <h2><?=$d->categoria_nome?></h2>
+                  </div>
+        <?php
+                  $titulo = $d->categoria;
+              }
+        ?>
           <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out">
             <div class="service-item position-relative w-100">
               <div class="icon d-flex justify-content-between">
