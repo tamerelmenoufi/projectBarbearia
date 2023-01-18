027fedcafed1cbcd68d70964c10b307d096d980f
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-18 15:40:40 -0400

    Atualizações

--- _modelo/components/produtos_servicos.php
+++ _modelo/components/produtos_servicos.php
@@ -11,9 +11,8 @@
             ?>
           <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out">
             <div class="service-item position-relative w-100">
-              <div class="icon">
-              <img src="<?=$localPainel?>src/volume/produtos/<?=$d->imagem?>" style="height:80px;" class="img-fluid" alt="">
-                <!-- <i class="bi bi-activity icon"></i> -->
+              <div class="icon d-flex justify-content-between">
+                <img src="<?=$localPainel?>src/volume/produtos/<?=$d->imagem?>" style="height:80px;" class="img-fluid" alt="">
                 <span>R$ 12,00</span>
               </div>
               <h4><a href="" class="stretched-link"><?=$d->produto?></a></h4>
