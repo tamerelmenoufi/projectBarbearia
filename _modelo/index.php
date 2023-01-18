febed1a5f40ab23fa63816f3f4c7c9598f14480d
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-18 10:10:26 -0400

    Atualizações

--- _modelo/index.php
+++ _modelo/index.php
@@ -66,21 +66,17 @@
 
 <body>
 
-<div
-    class="offcanvas offcanvas-end"
-    data-bs-backdrop="static"
-    tabindex="-1"
-    id="offcanvasDireita"
-    aria-labelledby="offcanvasDireitaLabel"
-    style="--bs-offcanvas-width:500px;"
-  >
+<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Toggle right offcanvas</button>
+
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
   <div class="offcanvas-header">
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    <h5 id="offcanvasRightLabel">Offcanvas right</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body LateralDireita"></div>
+  <div class="offcanvas-body">
+    ...
+  </div>
 </div>
-
-
 
 <main id="main">
 <?php
