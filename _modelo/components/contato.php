7d4edc862911bbb56d7f379338bf03d7660a38f7
livia128924 <livia128924@gmail.com>
2023-01-19 14:34:15 -0300

    atualizando

--- _modelo/components/contato.php
+++ _modelo/components/contato.php
@@ -25,7 +25,7 @@
       </div>
 
       <div class="">
-        <iframe src="<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.9342913523174!2d-60.027689584222976!3d-3.1120879350110138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c051e5797af1f%3A0x15df2fe63677b86b!2sOs%20Manos%20Barbearia!5e0!3m2!1spt-BR!2sbr!4v1674148746058!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>" frameborder="0" allowfullscreen></iframe> 
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3983.9342913523174!2d-60.027689584222976!3d-3.1120879350110138!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c051e5797af1f%3A0x15df2fe63677b86b!2sOs%20Manos%20Barbearia!5e0!3m2!1spt-BR!2sbr!4v1674149641605!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div><!-- End Google Maps -->
 
       <div class="container">
