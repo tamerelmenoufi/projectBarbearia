cdd898b9d62d159350a00c76faa3aae81be6190e
livia128924 <livia128924@gmail.com>
2023-01-19 15:46:02 -0300

    atualizando

--- painel/src/componentes/menu_lateral/menu.php
+++ painel/src/componentes/menu_lateral/menu.php
@@ -2,51 +2,68 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
     vl(['ProjectPainel']);
 ?>
+
+
+<style>
+.menu-cinza{
+  padding:8px;
+  font-size:15px;
+  border-bottom:1px solid #d7d7d7;
+  cursor:pointer;
+}
+
+.texto-cinza{
+  color:#5e5e5e;
+}
+
+</style>
+
+
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
     <img src="img/logo-topo.png" style="height:100px;" alt="">
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <h5>Project - Barbearia</h5>
+    <h5>Os Manos Barbearia</h5>
 
-    <div class="row mb-1">
+    <div class="row mb-1 menu-cinza">
       <div class="col">
-        <a url="src/dashboard/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+        <a url="src/dashboard/index.php" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-clipboard-list"></i> Dashboard
         </a>
       </div>
     </div>
 
 
-    <div class="row mb-1">
+    <div class="row mb-1  menu-cinza">
       <div class="col">
-        <a url="src/vendas/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+        <a url="src/vendas/index.php" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-clipboard-list"></i> Vendas
         </a>
       </div>
     </div>
 
 
-    <div class="row mb-1">
+    <div class="row mb-1 menu-cinza">
       <div class="col">
-        <a url="src/colaboradores/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+        <a url="src/colaboradores/index.php" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-clipboard-list"></i> Usuários/Colaboradores
         </a>
       </div>
     </div>
 
-    <div class="row mb-1">
+    <div class="row mb-1 menu-cinza">
       <div class="col">
-        <a url="src/clientes/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+        <a url="src/clientes/index.php" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-clipboard-list"></i> Clientes
         </a>
       </div>
     </div>
 
-    <div class="row mb-1">
+    <div class="row mb-1 menu-cinza">
       <div class="col">
-        <a url="src/produtos_categorias/index.php" class="text-decoration-none" data-bs-dismiss="offcanvas" aria-label="Close">
+        <a url="src/produtos_categorias/index.php" class="text-decoration-none texto-cinza" data-bs-dismiss="offcanvas" aria-label="Close">
           <i class="fa-solid fa-clipboard-list"></i> Produtos / Serviços
         </a>
       </div>
