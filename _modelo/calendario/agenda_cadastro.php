4e4c9cc20f827f1cf02059c63820a4f2f8c0123d
livia128924 <livia128924@gmail.com>
2023-01-19 18:11:07 -0300

    atualizando

--- _modelo/calendario/agenda_cadastro.php
+++ _modelo/calendario/agenda_cadastro.php
@@ -78,6 +78,25 @@
 
 
 
+.form-control {
+    display: block;
+    width: 100%;
+    /* padding: 0.375rem 0.75rem; */
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #fff;
+    background-color: #482e1500;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    border-radius: 0.25rem;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    height: 85px;
+}
+
 </style>
 <h4 class="Titulo<?=$md5?>"><b style="color:#fff">Agenda <span Titulo></span></b></h4>
 
