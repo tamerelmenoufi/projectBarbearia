c936c45ba47f79e93b8dbf88ec83150fae15595a
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-22 22:19:40 -0400

    Atualizações

--- _modelo/components/produtos_servicos2.php
+++ _modelo/components/produtos_servicos2.php
@@ -21,16 +21,10 @@
         ?>
 
           <div class="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
-            <div class="service-item"
-                 style="
-                  background-image: url(<?=$localPainel?>src/volume/colaboradores/<?=$d->foto?>);
-                  background-size:contain;
-                  background-repeat:no-repeat;
-                 "
-            >
-              <!-- <div class="img">
+            <div class="service-item">
+              <div class="img">
                 <img src="<?=$localPainel?>src/volume/colaboradores/<?=$d->foto?>" class="img-fluid" alt="" style="width:100%;">
-              </div> -->
+              </div>
               <div class="details position-relative">
                 <div class="icon">
                   <i class="bi bi-activity"></i>
