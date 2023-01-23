da9df000fede1467fd9cca749dba3bde1eef2311
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-22 23:15:21 -0400

    Atualizações

--- _modelo/components/produtos_servicos.php
+++ _modelo/components/produtos_servicos.php
@@ -10,25 +10,28 @@
               $result = mysqli_query($con, $query);
               while($d = mysqli_fetch_object($result)){
         ?>
-          <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out">
-            <div class="service-item position-relative w-100"
+            <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out">
+              <div class="service-item position-relative w-100"
 
-              style="
-                  background-image: url(assets/img/foto_fundo.png);
-                  background-size:100% 100%;
-                  background-repeat:no-repeat;
-                  background-position:center center;
-                 "
-            >
-              <div class="d-flex justify-content-between">
-                <!-- <img src="<?=$localPainel?>src/volume/produtos/<?=$d->imagem?>" style="height:80px;" class="img-fluid" alt=""> -->
-                <span style="color:orange">
-                  <i class="fa fa-calendar"></i> Agendar
-                </span>
-                <span style="color:#a1a1a1; font-weight:bold;">R$ <?=number_format($d->valor,2,',','.')?></span>
-              </div>
+                style="
+                    background-image: url(assets/img/foto_fundo.png);
+                    background-size:100% 100%;
+                    background-repeat:no-repeat;
+                    background-position:center center;
+                  "
+              >
+
               <h4><a href="#servicos" class="stretched-link agenda_servico" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><?=$d->produto?></a></h4>
-              <p></p>
+
+
+              <div class="d-flex justify-content-between">
+                  <!-- <img src="<?=$localPainel?>src/volume/produtos/<?=$d->imagem?>" style="height:80px;" class="img-fluid" alt=""> -->
+                  <span style="color:orange">
+                    <i class="fa fa-calendar"></i> Agendar
+                  </span>
+                  <span style="color:#a1a1a1; font-weight:bold;">R$ <?=number_format($d->valor,2,',','.')?></span>
+              </div>
+
             </div>
           </div><!-- End Service Item -->
           <?php
