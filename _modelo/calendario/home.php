6271d8b982ea2a653bd4bf3bf35d09b39d867670
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-24 08:25:27 -0400

    Atualizações

--- _modelo/calendario/home.php
+++ _modelo/calendario/home.php
@@ -2,7 +2,7 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
 
     if($_POST['servico']) $_SESSION['servico'] = $_POST['servico'];
-    // $_SESSION['cliente']  = false;
+    $_SESSION['cliente']  = false;
     if($_SESSION['cliente']){
         $url = "calendario/agenda_cadastro.php";
     }else{
