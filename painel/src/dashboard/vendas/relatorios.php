8816c29c5246778d6a657c2266ce11a34a84e03c
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-25 17:34:16 -0400

    Atualizações

--- painel/src/dashboard/vendas/relatorios.php
+++ painel/src/dashboard/vendas/relatorios.php
@@ -25,7 +25,7 @@
 
     <div class="accordion-item">
         <h2 class="accordion-header" id="flush-headingOneXXXX">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOneXXXX" aria-expanded="false" aria-controls="flush-collapseOne">
             Total de Vendas
         </button>
         </h2>
