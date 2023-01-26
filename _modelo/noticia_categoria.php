f6a9dda2fcb0ec49785ba5b3299b60d729db8211
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-26 15:56:55 -0400

    Atualizações

--- _modelo/noticia_categoria.php
+++ _modelo/noticia_categoria.php
@@ -1,5 +1,5 @@
 <?php
-    include("{$_SERVER['DOCUMENT_ROOT']}/portal/site/assets/lib/includes.php");
+    include("{$_SERVER['DOCUMENT_ROOT']}/app/projectBarbearia/painel/lib/includes.php");
 ?><!DOCTYPE html>
 <html lang="en">
 
