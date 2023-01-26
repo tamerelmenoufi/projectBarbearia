3072fa20b51aa2d33bca36e5403a5b9b1090e8f3
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-26 11:56:18 -0400

    Atualizações

--- painel/src/noticias/form.php
+++ painel/src/noticias/form.php
@@ -90,7 +90,7 @@
       </div>
 
       <div showImage class="form-floating" style="display:<?=(($d->imagem)?'block':'none')?>">
-        <img src="<?=$localPainel?>src/volume/noticias/<?=$d->imagem?>" class="img-fluid mt-3 mb-3" alt="" />
+        <img src="<?=$localPainel?>src/noticias/<?=$d->imagem?>" class="img-fluid mt-3 mb-3" alt="" />
       </div>
 
       <!-- <div class="form-floating"> -->
