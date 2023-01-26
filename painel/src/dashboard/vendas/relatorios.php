1ce5951d8e3275fafa0dacda95aeb56476f8dbac
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-26 16:21:43 -0400

    Atualizações

--- painel/src/dashboard/vendas/relatorios.php
+++ painel/src/dashboard/vendas/relatorios.php
@@ -21,51 +21,123 @@
     <li class="list-group-item relatorio p-3">Estatísticas dos produtos</li>
 </ul> -->
 
-    <div class="accordion accordion-flush" id="accordionFlushExample">
+<div class="accordion accordion-flush" id="accordionFlushExample">
 
     <div class="accordion-item p-3">
         <div class="d-flex justify-content-between">
-            <div class="col text-start">Total de Vendas</div>
+            <div class="col text-start">Total de Agendas</div>
+            <div class="col text-end">232</div>
+        </div>
+    </div>
+    <div class="accordion-item p-3">
+        <div class="d-flex justify-content-between">
+            <div class="col text-start">Agendas Atendidas</div>
+            <div class="col text-end">232</div>
+        </div>
+    </div>
+    <div class="accordion-item p-3">
+        <div class="d-flex justify-content-between">
+            <div class="col text-start">Agendas não Atendidas</div>
             <div class="col text-end">232</div>
         </div>
     </div>
 
+    <div class="accordion-item p-3">
+        <div class="d-flex justify-content-between">
+            <div class="col text-start">Total de vendas</div>
+            <div class="col text-end">232</div>
+        </div>
+    </div>
+
+
+
+
+
     <div class="accordion-item">
-        <h2 class="accordion-header" id="flush-headingOne">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        <h2 class="accordion-header" id="flush-venda_servico">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-venda_servico" aria-expanded="false" aria-controls="flush-venda_servico">
 
             <div class="d-flex justify-content-between w-100 me-3">
-                <div class="col text-start">Accordion Item #1</div>
+                <div class="col text-start">Vendas por serviços</div>
                 <div class="col text-end">232</div>
             </div>
 
         </button>
         </h2>
-        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+        <div id="flush-venda_servico" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
         </div>
     </div>
+
     <div class="accordion-item">
-        <h2 class="accordion-header" id="flush-headingTwo">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-            Accordion Item #2
+        <h2 class="accordion-header" id="flush-vendas_produtos">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-vendas_produtos" aria-expanded="false" aria-controls="flush-vendas_produtos">
+
+            <div class="d-flex justify-content-between w-100 me-3">
+                <div class="col text-start">Vendas por produtos</div>
+                <div class="col text-end">232</div>
+            </div>
+
         </button>
         </h2>
-        <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
+        <div id="flush-vendas_produtos" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
         </div>
     </div>
+
+
     <div class="accordion-item">
-        <h2 class="accordion-header" id="flush-headingThree">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-            Accordion Item #3
+        <h2 class="accordion-header" id="flush-vendas_colaborador">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-vendas_colaborador" aria-expanded="false" aria-controls="flush-vendas_colaborador">
+
+            <div class="d-flex justify-content-between w-100 me-3">
+                <div class="col text-start">Vendas por colaborador</div>
+                <div class="col text-end">232</div>
+            </div>
+
         </button>
         </h2>
-        <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+        <div id="flush-vendas_colaborador" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
         </div>
     </div>
+
+
+
+
+    <div class="accordion-item p-3">
+        <div class="d-flex justify-content-between">
+            <div class="col text-start">Financeiro Bruto</div>
+            <div class="col text-end">232</div>
+        </div>
     </div>
+
+    <div class="accordion-item p-3">
+        <div class="d-flex justify-content-between">
+            <div class="col text-start">Financeiro comissão</div>
+            <div class="col text-end">232</div>
+        </div>
+    </div>
+
+
+    <div class="accordion-item">
+        <h2 class="accordion-header" id="flush-comissao_colaborador">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-comissao_colaborador" aria-expanded="false" aria-controls="flush-comissao_colaborador">
+
+            <div class="d-flex justify-content-between w-100 me-3">
+                <div class="col text-start">Comissão por colaborador</div>
+                <div class="col text-end">232</div>
+            </div>
+
+        </button>
+        </h2>
+        <div id="flush-comissao_colaborador" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+        </div>
+    </div>
+
+
+</div>
 
 
 <script>
