af0ab6d228b3b4eb10df51ad5d6d88e3933b0e73
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-27 08:57:17 -0400

    Atualizações

--- painel/src/dashboard/vendas/relatorios.php
+++ painel/src/dashboard/vendas/relatorios.php
@@ -121,7 +121,7 @@
         if(!$n){
         ?>
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                <h4>CATEGORIA SEM REGISTROS</h4>
+                <h4 style="text-align:center; color:#a1a1a1;">CATEGORIA SEM REGISTROS</h4>
             </li>
         <?php
         }
