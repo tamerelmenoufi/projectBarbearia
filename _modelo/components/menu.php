6391af486c490265895cf2b65963e2dfe9ea048c
livia128924 <livia128924@gmail.com>
2023-01-31 10:24:56 -0300

    atualizando

--- _modelo/components/menu.php
+++ _modelo/components/menu.php
@@ -4,7 +4,7 @@
 
       <a href="index.php" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <img src="assets/img/logo.png" alt="">
+        <img src="assets/img/logobarb.png" alt="">
       </a>
 
       <nav id="navbar" class="navbar">
