cf034a7a32fec18f3247333344eaa8fc9fae8a15
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-02 09:41:40 -0400

    Atualizações

--- _modelo/components/produtos.php
+++ _modelo/components/produtos.php
@@ -6,19 +6,19 @@
         </div>
         <div class="row gy-4">
         <?php
-              $query = "select a.*, b.categoria as categoria_nome from produtos a left join produtos_categorias b on a.categoria = b.codigo where a.situacao = '1' and a.tipo='p' order by a.tipo desc";
+              $query = "select a.*, (saldo - vendas) as saldo, b.categoria as categoria_nome from produtos a left join produtos_categorias b on a.categoria = b.codigo where a.situacao = '1' and a.tipo='p' order by a.tipo desc";
               $result = mysqli_query($con, $query);
               while($d = mysqli_fetch_object($result)){
         ?>
           <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out">
             <div class="service-item position-relative w-100">
               <div class="icon d-flex justify-content-between">
-                <img src="<?=$localPainel?>src/volume/produtos/<?=$d->imagem?>" style="height:80px; opacity:<?=(($d->estoque > 0)?'1':'0.3')?>;" class="img-fluid" alt="">
-                <span style="color:<?=(($d->estoque)?'#a1a1a1':'red')?>; font-weight:bold;"><?=(($d->estoque > 0)?'R$ '.number_format($d->valor,2,',','.'):'Indisponível')?></span>
+                <img src="<?=$localPainel?>src/volume/produtos/<?=$d->imagem?>" style="height:80px; opacity:<?=(($d->saldo > 0)?'1':'0.3')?>;" class="img-fluid" alt="">
+                <span style="color:<?=(($d->saldo)?'#a1a1a1':'red')?>; font-weight:bold;"><?=(($d->saldo > 0)?'R$ '.number_format($d->valor,2,',','.'):'Indisponível')?></span>
               </div>
               <h4><a
                     href="#XXX"
-                    class="stretched-link <?=(($d->estoque > 0)?'ver_produto':false)?>"
+                    class="stretched-link <?=(($d->saldo > 0)?'ver_produto':false)?>"
                     cod="<?=$d->codigo?>"
                     data-bs-toggle="offcanvas"
                     data-bs-target="#offcanvasRight"
