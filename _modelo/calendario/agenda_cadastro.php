30aeb88cd1c90a85e94202227bafc52ac436a9ea
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-07 17:33:34 -0400

    Atualizações

--- _modelo/calendario/agenda_cadastro.php
+++ _modelo/calendario/agenda_cadastro.php
@@ -155,20 +155,19 @@
 
         $(".opcHoras").click(function() {
 
-alert('dentro');
 
-        // ano = $(this).attr("ano");
-        // mes = $(this).attr("mes");
-        // dia = $(this).attr("dia");
-        // hora = $(this).val();
+            ano = $(this).attr("ano");
+            mes = $(this).attr("mes");
+            dia = $(this).attr("dia");
+            hora = $(this).val();
 
-        // agenda = `${ano}-${mes}-${dia} ${hora}`;
-        // rotulo = `${dia}/${mes}/${ano} ${hora}`;
+            agenda = `${ano}-${mes}-${dia} ${hora}`;
+            rotulo = `${dia}/${mes}/${ano} ${hora}`;
 
-        // $(".cadastrarAgenda").attr("agenda",agenda);
-        // $(".cadastrarAgenda span").text(rotulo);
-        // $(".cadastrarAgenda").removeAttr("disabled");
-        // $("span[Titulo]").text(rotulo);
+            $(".cadastrarAgenda").attr("agenda",agenda);
+            $(".cadastrarAgenda span").text(rotulo);
+            $(".cadastrarAgenda").removeAttr("disabled");
+            $("span[Titulo]").text(rotulo);
 
         });
     })
