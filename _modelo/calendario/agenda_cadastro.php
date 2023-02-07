66d5ce169e3bbb07d81f71866212449a0f6d49a2
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-07 11:30:46 -0400

    Atualizações

--- _modelo/calendario/agenda_cadastro.php
+++ _modelo/calendario/agenda_cadastro.php
@@ -92,7 +92,7 @@
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title"><b><?=$d->produto_nome?></b> <i>com:</i> <?=$d->colaborador_nome?></h5>
+        <h5 class="card-title"><b><?=$d->produto_nome?></b> <i>com:</i><br><?=$d->colaborador_nome?></h5>
         <p class="card-text">
 
         <?php
