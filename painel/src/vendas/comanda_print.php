8dad4d06fcdfc27c1dabbb032fddcd44dfa1da6d
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-23 00:54:14 +0200

    Atualizações

--- painel/src/vendas/comanda_print.php
+++ painel/src/vendas/comanda_print.php
@@ -234,11 +234,4 @@
         Carregando('none');
     })
 
-    var conteudo = document.getElementById('imprimir').innerHTML,
-    tela_impressao = window.open('about:blank');
-
-    tela_impressao.document.write(conteudo);
-    tela_impressao.window.print();
-    tela_impressao.window.close();
-
 </script>
