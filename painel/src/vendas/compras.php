55ca98d6dd0d028b65225507295601526d4539f4
tamerelmenoufi <tamer@mohatron.com.br>
2023-03-22 18:31:42 +0200

    Atualizações

--- painel/src/vendas/compras.php
+++ painel/src/vendas/compras.php
@@ -182,7 +182,6 @@
                 $comissao = $comissao + $d->comissao;
             }
 
-
             $q = "update vendas set
                                                 valor = '{$valor}',
                                                 comissao = '{$comissao}',
